1ead2b14fb4dcb0e100a221a3299a7792ed71e16
dmytro-kvasov <dmytro.kvasov@advantrix-labs.com>
2026-06-17 11:05:28 +0200

    commit

--- app/components/GoogleTag.tsx
+++ app/components/GoogleTag.tsx
@@ -2,6 +2,12 @@ import Script from 'next/script'
 
 const GOOGLE_ADS_ID = 'AW-18225665896'
 
+declare global {
+  interface Window {
+    gtag_report_conversion?: (url?: string) => boolean
+  }
+}
+
 
 export function GoogleTag() {
   return (
@@ -17,6 +23,34 @@ export function GoogleTag() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', '${GOOGLE_ADS_ID}');
+        `}
+      </Script>
+      <Script id="google-tag-conversion" strategy="afterInteractive">
+        {`
+          function gtag_report_conversion(url) {
+            var callback = function () {
+              if (typeof(url) != 'undefined') {
+                window.location = url;
+              }
+            };
+            gtag('event', 'conversion', {
+                'send_to': 'AW-18225665896/DmwsCJzA2sAcEOiu1vJD',
+                'event_callback': callback
+            });
+            return false;
+          }
+        `}
+      </Script>
+      <Script id="google-tag-waitlist-cta" strategy="afterInteractive">
+        {`
+          document.addEventListener('click', function (e) {
+            var t = e.target;
+            if (!t || typeof t.closest !== 'function') return;
+            var link = t.closest('a[href="#waitlist"]');
+            if (link && typeof gtag_report_conversion === 'function') {
+              gtag_report_conversion();
+            }
+          });
         `}
       </Script>
     </>
